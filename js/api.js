19b55432347e42965ceda9b40ca778733b59b333
Aabis5004 <88813561+Aabis5004@users.noreply.github.com>
2025-09-07 15:28:29 +0530

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 class GameAPI {
     constructor() {
         // Your VPS IP is configured here!
-       this.baseURL = 'http://aztecgame.duckdns.org/api';
+      this.baseURL = 'https://aztecgame.duckdns.org/api';
         this.token = localStorage.getItem('aztec-game-token');
         console.log('🔧 API initialized with baseURL:', this.baseURL);
     }
